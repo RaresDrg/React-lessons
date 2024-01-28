2af09aae905c6de66fca3c0ac4ef116a4bcda487
RaresDrg <dragan_rares13@yahoo.com>
2024-01-28 05:47:44 +0200

    lesson-04
    lesson-05

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,33 +1,24 @@
-import React, { Component } from "react";
-import "./App.css";
-import Sidebar from "./components/Sidebar/Sidebar";
-import University from "./components/University/University";
-import Tutors from "./components/Tutors/Tutors";
+import { Component } from 'react'
+import './App.css'
+import Sidebar from './components/Sidebar/Sidebar'
+import Tutors from './components/Tutors/Tutors'
+import University from './components/University/University'
+import Cities from './components/Cities/Cities'
 
 class App extends Component {
-  tutors = [
-    {
-      id: 1,
-      firstName: "John",
-      lastName: "Smith",
-      telephone: "07123456",
-      email: "johnsmith@company.com",
-      location: "Paris",
-      role: "Administrator",
-    },
-  ];
 
   render() {
     return (
-      <main className="App">
-        <Sidebar />
+      <main className='App'>
+        <Sidebar/>
         <section className="container">
-          <University />
-          <Tutors list={this.tutors} />
+          <University/>
+          <Tutors/>
+          <Cities/>
         </section>
       </main>
-    );
+    )
   }
 }
 
-export default App;
+export default App
